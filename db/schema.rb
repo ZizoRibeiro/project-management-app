5fdcb4c878de24db8a498a7d938c312d910c4dc4
Zizo Ribeiro <zizo.ribeiro@gmail.com>
2016-07-09 11:57:11 -0700

    Added artifact gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160707170553) do
+ActiveRecord::Schema.define(version: 20160708172256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artifacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "key"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "members", force: :cascade do |t|
     t.integer  "tenant_id"
